318b059406ea4288c80e68511ffc5642247e25ef
printf-twinkle <twinklechokostar@gmail.com>
2024-07-20 00:14:24 +0530

    some changes referring mentor repo

--- src/main.ts
+++ src/main.ts
@@ -5,23 +5,26 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe())
   const config = new DocumentBuilder()
   .setTitle('Todo REST API')
-  .setDescription("A rest api to list user's todos/tasks, authorization implemented using JWT token. The API is made using Nest JS")
+  .setDescription("A rest api to list user's todos/tasks, authorization implemented using JWT token. THe API is made using NestJs.")
   .setVersion('1.0')
   .addBearerAuth()
   .build();
 const document = SwaggerModule.createDocument(app, config);
 SwaggerModule.setup('/swagger', app, document, {
-  customCssUrl:
-  'https://cdnjs.cloudfare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
-  customJs: [
-    'https://cdnjs.cloudfare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
-    'https://cdnjs.cloudfare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalaone-preset.js',
-  ],
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
 });
-await app.listen(3000);
+  await app.listen(3000);
 }
 bootstrap();
